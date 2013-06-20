48b20e071cb02bd327300766dd9fb520e6ffcb63
qiangjishen <qiangjishen@gmail.com>
2013-06-20 11:29:32 +0800

    Update gae.py

--- gae.py
+++ gae.py
@@ -2,6 +2,7 @@
 
 import urllib2
 import webbrowser
+
 proxy_support = urllib2.ProxyHandler({'https':'http://127.0.0.1:8087'})
 opener = urllib2.build_opener(proxy_support,urllib2.HTTPHandler)
 urllib2.install_opener(opener)
